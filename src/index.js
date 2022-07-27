c9f6ff4c93c4e43bba56af97985d6dbf55e5b6b9
Superflows <108924653+Superflow-3@users.noreply.github.com>
2022-07-27 17:57:57 +0530

    Indication of multiselect to the user

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Col, Row, Container } from 'react-bootstrap';
 import { QuestionSquare } from 'react-bootstrap-icons';
 
 export const McqView = (props) => {
-  
+
   const [value, setValue] = useState(props.selectedValue == null ? [] : props.selectedValue.length === 0 ? [] : JSON.stringify(props.selectedValue));
 
   useEffect(() => {
@@ -47,12 +47,24 @@ export const McqView = (props) => {
 
     <Container className="w-100 border rounded-3 bg-white border-secondary" >
       <Row className='justify-content-center'>
-        <Col sm={12} xs={12} md={12} xxl={12} className={`d-flex flex-wrap align-items-center p-2 text-secondary`} >
+        <Col sm={12} xs={12} md={12} xxl={12} className={`d-flex flex-wrap align-items-center p-2`} >
           <QuestionSquare className='me-3 ms-2'/>
-          <div className='pe-3 border-end flex-grow-1'>
+          <div className='pe-3 flex-grow-1'>
               {
                   props.question
               }
+          </div>
+        </Col>
+      </Row>
+      <Row className='justify-content-center'>
+        <Col sm={12} xs={12} md={12} xxl={12} className={`d-flex flex-wrap align-items-center px-2 text-muted`} >
+          <QuestionSquare className='me-3 ms-2' style={{visibility: 'hidden'}}/>
+          <div className='d-flex flex-grow-1 text-small justify-content-end'>
+            <small>
+              {
+                props.multiSelect ? "Choose one or more" : "Choose one"
+              }
+            </small>
           </div>
         </Col>
       </Row>
